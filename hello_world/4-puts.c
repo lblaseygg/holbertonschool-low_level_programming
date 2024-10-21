c2285407fd7ea92a1539b01aca00e0dd960b0b44
blasey <justblasey@gmail.com>
2024-10-21 19:09:31 -0400

    Fixing task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,17 @@
+/*
+ * By Luis Feliciano
+ */
+
 #include <stdio.h>
+
 /**
- * main: Prints "Programming is like building a multilingual puzzle" followed by a new line.
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle", followed by a new line.
  *
- * Description: This program prints a specific message
- * to the standard output using the puts function.
- *
- * Return: ALways 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
