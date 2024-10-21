1e66bb92f938c9333337ce72e37c1b878a6ceb54
blasey <justblasey@gmail.com>
2024-10-21 19:02:10 -0400

    fixing task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
-/*
- * by Luis F
- * main prints a message
- * if succesful it returns 0
- */
-
 #include <stdio.h>
+/**
+ * main = entry point of the program
+ *
+ * This program prints a specific message
+ * to the standard output using the puts function.
+ *
+ * Return 0 if succesful
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
