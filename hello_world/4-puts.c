ac1e7261fbae5b19a08cc1ae4cf0f9e114e3e9c0
blasey <justblasey@gmail.com>
2024-10-21 18:57:38 -0400

    Testing C

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ * by Luis F
+ * main prints a message
+ * if succesful it returns 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
