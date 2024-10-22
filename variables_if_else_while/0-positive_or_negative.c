05e977ffbc3e300b8bdbc54a1394d0f1969de3f0
blasey <justblasey@gmail.com>
2024-10-22 11:20:22 -0400

    Testing C code for task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* Prints: if N is positive, negative or zero */
 int main(void)
 {
 	int n;
