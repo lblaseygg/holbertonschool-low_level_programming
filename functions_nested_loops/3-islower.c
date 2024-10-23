2762337ab8722389eab7bf5ee7699c36ba6fa9f7
blasey <justblasey@gmail.com>
2024-10-23 13:17:44 -0400

    Testing code for task

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/*
+ * By Luis Feliciano
+ */
+
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
