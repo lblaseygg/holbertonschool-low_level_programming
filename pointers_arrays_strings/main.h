60cf22dde38c36c829477559781bd52b8ddf1536
blasey <justblasey@gmail.com>
2024-11-02 23:45:23 -0400

    Writing a function that reverses a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int  _strlen(char *s);
-int _putchar(char c);
-void _puts(char *str);
-#endif
+void _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+#endif 
+
 
