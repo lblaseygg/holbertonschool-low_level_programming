44a6d8d8e338ee0e6327e67f2950987e6354b06b
blasey <justblasey@gmail.com>
2024-11-19 15:06:29 -0400

    Write a program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,40 +1,43 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Performs simple operations
- * @argc: The number of arguments passed to the program
- * @argv: The array of arguments passed to the program
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, or exits with an error code on failure
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	int (*operation)(int, int);
+	char *op;
 
-	if (argc != 4)  /* Check if the correct number of arguments is passed */
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", operation(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
